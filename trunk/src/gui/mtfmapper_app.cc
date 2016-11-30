b7cf2968a8085df40e1646670f4025987b84943b
fvdbergh <fvdbergh@f0b42d95-50d2-4a92-b73f-df2d32d21376>
2016-11-30 06:09:01 +0000

    incorporated opencv header changes that should allow code to build with opencv 3 many thanks to kriolog / Maxim for contributing the fix

--- trunk/src/gui/mtfmapper_app.cc
+++ trunk/src/gui/mtfmapper_app.cc
@@ -33,9 +33,9 @@ or implied, of the Council for Scientific and Industrial Research (CSIR).
 #include "worker_thread.h"
 #include "common.h"
 
-// OpenCV headers
-#include <cv.h>
-#include <highgui.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <string>
 #include <iostream>
