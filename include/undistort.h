88aabdb3187de16e353f21649ff6cb48719b81c1
fvdbergh <43442774+fvdbergh@users.noreply.github.com>
2017-07-05 12:46:08 +0000

    optimized the equiangular distortion functions by removing transcendental functions, and using a look-up table + quadratic interpolation instead

--- include/undistort.h
+++ include/undistort.h
@@ -74,16 +74,11 @@ class Undistort_equiangular : public Undistort {
         double py = (row + offset.y - centre.y)*pitch;
     
         double rd = sqrt((px)*(px) + (py)*(py)); // radial distance in mm
-        double phi = atan2(py, px);
-        
         double theta = atan(rd/rect_f);
         double ru = theta*f;
         
-        px = ru*cos(phi);
-        py = ru*sin(phi);
-        
-        px = px / pitch + centre.x - offset.x;
-        py = py / pitch + centre.y - offset.y;
+        px = px*ru/rd / pitch + centre.x - offset.x;
+        py = py*ru/rd / pitch + centre.y - offset.y;
         
         return cv::Point2d(px, py);
     }
@@ -93,26 +88,27 @@ class Undistort_equiangular : public Undistort {
         double py = (row + offset.y - centre.y)*pitch;
     
         double rd = sqrt((px)*(px) + (py)*(py)); // radial distance in mm
-        double phi = atan2(py, px);
-        
         double theta = rd/f;
         double ru = tan(theta)*rect_f;
         
-        px = ru*cos(phi);
-        py = ru*sin(phi);
-        
-        px = px / pitch + centre.x - offset.x;
-        py = py / pitch + centre.y - offset.y;
+        px = px*ru/rd / pitch + centre.x - offset.x;
+        py = py*ru/rd / pitch + centre.y - offset.y;
         
         return cv::Point2d(px, py);
     }
     
     // note: second parameter is the raw Bayer image, which must also be padded out
     cv::Mat unmap(const cv::Mat& in_src, cv::Mat& rawimg) {
-    
-        Point2d extreme = inverse_transform_point(0, 0);
+        
+        Point2d extreme = inverse_transform_point(0, in_src.rows/2);
         int pad_left = extreme.x < 0 ? ceil(-extreme.x) : 0;
+        
+        extreme = inverse_transform_point(in_src.cols/2, 0);
         int pad_top  = extreme.y < 0 ? ceil(-extreme.y) : 0;
+        
+        int pad = std::max(pad_left, pad_top);
+        pad_left = pad;
+        pad_top = pad;
         
         // to preserve Bayer CFA alignment
         pad_left += pad_left % 2;
@@ -130,32 +126,36 @@ class Undistort_equiangular : public Undistort {
             cv::Mat rcopy = rawimg.clone();
             copyMakeBorder(rcopy, rawimg, pad_top, pad_top, pad_left, pad_left, cv::BORDER_CONSTANT, cv::Scalar::all(0));
         }
-        
     
         cv::Mat map_x(src.rows, src.cols, CV_32FC1);
         cv::Mat map_y(src.rows, src.cols, CV_32FC1);
         
-        // not incredibly fast, but it is a start ...
+        double maxrad = sqrt(src.cols*src.cols/4 + src.rows*src.rows/4);
+        vector<double> radmap;
+        for (int rad=0; rad <= 2*(maxrad + 2); rad++) { // half-pixel spacing
+            double theta = atan(double(rad*0.5)*pitch/rect_f);
+            double r_d = theta*f;
+            radmap.push_back(r_d/pitch);
+        }
+        
         for (int r=0; r < src.rows; r++) {
             for (int c=0; c < src.cols; c++) {
                 
-                double px = (c + offset.x - centre.x)*pitch;
-                double py = (r + offset.y - centre.y)*pitch;
-            
-                double rd = sqrt((px)*(px) + (py)*(py)); // radial distance in mm
-                double phi = atan2(py, px);
+                double px = (c + offset.x - centre.x);
+                double py = (r + offset.y - centre.y);
                 
-                double theta = atan(rd/rect_f);
-                double rad = theta*f;
+                double rad = 2*sqrt(px*px + py*py); // convert to half-pixel spacing
+                int rad_f = (int)rad;
                 
-                px = rad*cos(phi);
-                py = rad*sin(phi);
+                double w = rad - rad_f;
+                // quadratic interpolation through half-pixel spaced points
+                double fc = radmap[rad_f];
+                double fb = -1.5*radmap[rad_f] + 2*radmap[rad_f+1] - 0.5*radmap[rad_f+2];
+                double fa =  0.5*radmap[rad_f] - radmap[rad_f+1] + 0.5*radmap[rad_f+2];
+                double rad_d = 2*(fa*w*w + fb*w + fc);
                 
-                px = px / pitch + centre.x - offset.x;
-                py = py / pitch + centre.y - offset.y;
-                
-                map_x.at<float>(r, c) = px;
-                map_y.at<float>(r, c) = py;
+                map_x.at<float>(r, c) = px/rad*rad_d + centre.x - offset.x;
+                map_y.at<float>(r, c) = py/rad*rad_d + centre.y - offset.y;
             }
         }
         cv::Mat timg;
