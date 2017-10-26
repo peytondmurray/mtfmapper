32816e2a39ac75105d867d7b914031512e8786a8
fvdbergh <43442774+fvdbergh@users.noreply.github.com>
2017-10-26 10:15:16 +0000

    reworked piecewise quadratic fit: inflection point detection added, upgraded to QR factorization solution

--- src/esf_sampler_piecewise_quad.cc
+++ src/esf_sampler_piecewise_quad.cc
@@ -36,8 +36,59 @@ inline double tricube(double d) {
 
 vector<double> Esf_sampler_piecewise_quad::piecewise_quadfit(const vector<Point2d>& pts) {
     /// assumes pts.size() >= 3 
-
     
+    // compute integral array, which will allow us to calculate
+    // box-filtered versions of pts in constant time
+    vector<Point2d> integral(pts.size()+1);
+    Point2d cumulative = pts.front();
+    for (size_t i=0; i < pts.size(); i++) {
+        integral[i] = cumulative;
+        cumulative += pts[i];
+    }
+    integral.back() = cumulative;
+    
+    const int nbins = 16; // seems reasonable relative to 2nd derivative filter below
+    vector<Point2d> reduced(nbins);
+    for (int i=0; i < nbins; i++) {
+        int li = std::max(0, int((i-1.5)*int(pts.size()) / nbins));
+        int ui = std::min((int)pts.size()-1, int((i+2.5)*int(pts.size()) / nbins));
+        
+        reduced[i] = (integral[ui] - integral[li]) * (1.0 / (ui - li));
+    }
+    
+    const int hw=3;
+    double sgw[2*hw+1] = {
+        5.952381e-02, 6.938894e-18, -3.571429e-02, -4.761905e-02, -3.571429e-02, 6.938894e-18, 5.952381e-02, 
+    };
+    
+    vector<Point2d> deriv(reduced.size());
+    for (int i=0; i < (int)reduced.size(); i++) {
+        double wsum = 0;
+        deriv[i].x = reduced[i].x;
+        deriv[i].y = 0;
+        for (int j=-hw; j <= hw; j++) {
+            int ei = i+j;
+            if (ei >= 0 && ei < (int)reduced.size()) {
+                wsum += sgw[j+hw];
+                deriv[i].y += sgw[j+hw] * reduced[ei].y;
+            }
+        }
+        deriv[i].y /= wsum;
+    }
+    
+    // find zero crossings, pick one closest to centre?
+    // how do we decide if we have multiple crossings?
+    int i1 = 0;
+    for (int i=0; i < (int)deriv.size()-1; i++) {
+        if (deriv[i].y*deriv[i+1].y < 0) {
+            if (fabs(deriv[i].x) < fabs(deriv[i1].x)) {
+                i1 = i;
+            }
+        }
+    }
+    // interpolate
+    double slope = (deriv[i1+1].x - deriv[i1].x)/(deriv[i1+1].y - deriv[i1].y);
+    double crossing = (0 - deriv[i1].y) * slope + deriv[i1].x;
     
     Eigen::MatrixXd A(pts.size(), 6);
     Eigen::VectorXd b(pts.size());
@@ -48,23 +99,24 @@ vector<double> Esf_sampler_piecewise_quad::piecewise_quadfit(const vector<Point2
     double span = fabs(pts.back().x - pts.front().x);
     double x_scale = span*0.5/sqrt(0.5);
     
-    const double T = 1.0/x_scale; // we could find T by looking for the peak in the second difference ??
+    if (crossing < pts.front().x + 0.3*span || crossing > pts.front().x + 0.7*span) {
+        crossing = 0;
+    } 
+    
+    double T = crossing/x_scale;
     
     // constraints: first row is f(T) = g(T), second row is f'(T) = g'(T)
     C << T*T, T, 1, -T*T, -T, -1,
          2*T, 1, 0, -2*T, -1,  0;
          
-    int lower_thresh = std::max(-span/3.0, -30.0)/x_scale;
-    int upper_thresh = std::min(span/3.0, 30.0)/x_scale;
-         
     for (size_t r=0; r < pts.size(); r++) {
         double sx = pts[r].x / x_scale;
-        double w1 = sx <= T ? 1 : sx < upper_thresh ? tricube((sx - T)/(upper_thresh - T)) : 0.0;
+        double w1 = sx <= T ? 1 : 0;
         A(r, 0) = w1*sx*sx;
         A(r, 1) = w1*sx;
         A(r, 2) = w1*1.0;
         
-        double w2 = sx > T ? 1 : sx > lower_thresh ? tricube((T - sx)/(T - lower_thresh)) : 0.0;
+        double w2 = sx > T ? 1 : 0;
         A(r, 3) = w2*sx*sx;
         A(r, 4) = w2*sx;
         A(r, 5) = w2*1.0;
@@ -72,28 +124,31 @@ vector<double> Esf_sampler_piecewise_quad::piecewise_quadfit(const vector<Point2
         b[r] = pts[r].y * (w1 + w2);
     }
     
-    Eigen::MatrixXd design(6 + 2, 6 + 2);
-    design.setZero();
-    design.topLeftCorner(6, 6) = A.transpose() * A;
-    design.bottomLeftCorner(2, 6) = C;
-    design.topRightCorner(6, 2) = C.transpose();
+    // null space method
+    int n = A.cols();
+    int p = C.rows();
+    auto qr1 = C.transpose().householderQr();
+    Eigen::MatrixXd Q = qr1.householderQ();
+    Eigen::MatrixXd R = qr1.matrixQR().topRows(p).template triangularView<Eigen::Upper>();
     
-    Eigen::VectorXd b_d(6 + 2);
-    b_d.setZero();
-    b_d.topRows(6) = A.transpose() * b;
+    Eigen::VectorXd d = Eigen::VectorXd::Zero(p);
+    Eigen::VectorXd y1 = R.transpose().triangularView<Eigen::Lower>().solve(d);
     
-    Eigen::VectorXd sol = design.colPivHouseholderQr().solve(b_d);
+    Eigen::VectorXd x1 = Q.leftCols(p)*y1;
+    Eigen::VectorXd y2 = (A*Q.rightCols(n - p)).colPivHouseholderQr().solve(b - A*x1);
+    Eigen::VectorXd sol = x1 + Q.rightCols(n - p)*y2;
     
     sol[0] /= x_scale*x_scale;
     sol[1] /= x_scale;
     sol[3] /= x_scale*x_scale;
     sol[4] /= x_scale;
+    T *= x_scale;
     
     // sol[0-5] : quadratic parameters
     // sol[6-7] : constraints values (not used)
     
     vector<double> parms(7);
-    parms[0] = T * x_scale;
+    parms[0] = T;
     parms[1] = sol[0];
     parms[2] = sol[1];
     parms[3] = sol[2];
