a9db3da5312e69427a7724cc2f052d2cf7876062
fvdbergh <43442774+fvdbergh@users.noreply.github.com>
2020-01-29 06:30:02 +0000

    refactored ratpoly to separate implementation from header

--- include/ratpoly_fit.h
+++ include/ratpoly_fit.h
@@ -34,6 +34,8 @@ or implied, of the Council for Scientific and Industrial Research (CSIR).
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+#include "include/laguerre_roots.h"
+
 class Sample {
   public:
     Sample(double x, double y, double weight, double yweight=1) 
@@ -56,96 +58,6 @@ class Ratpoly_fit  {
     : data(data), order_n(order_n), order_m(order_m), base_value(1.0), 
       xsf(1), ysf(1), pscale(0.1), evaluation_count(0) {}
     
-    double evaluate(VectorXd& v) {
-        double err = 0;
-        for (size_t i=0; i < data.size(); i++) {
-            double w = data[i].weight * data[i].yweight;
-            double z = rpeval(v, data[i].x*xsf);
-            double e = data[i].y*ysf - z;
-            err += e*e*w;
-        }
-        evaluation_count++;
-        return err*0.5;
-    }
-    
-    VectorXd gauss_newton_direction(VectorXd& v, VectorXd& deriv, double& fsse) {
-        MatrixXd J(data.size(), v.rows());
-        J.setZero();
-        fsse = 0; 
-        
-        VectorXd r(data.size());
-        for (size_t m=0; m < data.size(); m++) {
-            double w = data[m].weight * data[m].yweight;
-            double fx = 0;
-            
-            J.row(m) = rp_deriv(v, data[m].x*xsf, fx); 
-            double e = fx - data[m].y*ysf;
-            r[m] = e*w;
-            fsse += e*e*w;
-        }
-        
-        
-        VectorXd direction = J.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(-r);
-        
-        fsse *= 0.5;
-        deriv = J.transpose() * r;
-        evaluation_count++;
-        return direction;
-    }
-    
-    VectorXd evaluate_derivative(VectorXd& v) {
-        // just assume "fit==LS" for now
-        
-        VectorXd deriv(v.rows());
-        deriv.setZero();
-        VectorXd d(v.rows());
-        for (size_t i=0; i < data.size(); i++) {
-            double w = data[i].weight * data[i].yweight;
-            double z=0;
-            
-            d.setZero();
-            if (rp_deriv_eval(v, data[i].x*xsf, d, z)) {
-                double e = z - data[i].y*ysf;
-                deriv += e*d*w;
-            } else {
-                logger.debug("hit a pole!\n");
-            }
-        }
-        evaluation_count++;
-        return deriv;
-    }
-    
-    VectorXd gauss_newton_armijo(VectorXd& v) {
-        const double tau = 0.5;
-        const double c = 1e-4;
-        double fx = 0;
-        
-        VectorXd grad;
-        VectorXd next;
-        VectorXd pk;
-        for (int k=0; k < 50; k++) {
-            
-            double alpha = 1.0;
-            pk = gauss_newton_direction(v, grad, fx);
-            
-            double target = fx + c*alpha*pk.dot(grad);
-            
-            int max_steps = 30;
-            next = v + alpha*pk;
-            while (evaluate(next) > target && --max_steps > 0) { // iteratively step close until we have a sufficient decrease (Armijo condition)
-                target = fx + c*alpha*pk.dot(grad);
-                alpha *= tau;
-                next = v + alpha*pk;
-            }
-            
-            double stepsize = pk.array().abs().maxCoeff()*fabs(alpha);
-            if (stepsize < 5e-8) {
-                break;
-            }
-            v = next;
-        }
-        return v;
-    }
     
     virtual int dimension(void) {
         return (order_n+1 + order_m);
@@ -241,101 +153,16 @@ class Ratpoly_fit  {
         return (dp*bot_val - top_val*dq) * den;
     }
     
-    double peak(const VectorXd& v) {
-        double xmin=1e50;
-        double xmax=-1e50;
-        for (size_t i=0; i < data.size(); i++) {
-            xmin = min(data[i].x, xmin);
-            xmax = max(data[i].x, xmax);
-        }
-        // bracket the maximum
-        double peak_z = 0;
-        double peak_x = (xmin + xmax)*0.5;
-        double step = (xmax - xmin)/20.0;
-        for (double x=xmin; x <= xmax; x += step) {
-            double z = rpeval(v, x*xsf);
-            if (z > peak_z) {
-                peak_x = x;
-                peak_z = z;
-            }
-        }
-        
-        // golden section search
-        const double phi = 0.61803398874989;
-        double lower = peak_x - 2*step;
-        double upper = peak_x + 2*step;
-        double c = upper - phi*(upper - lower);
-        double d = lower + phi*(upper - lower);
-        const double tol = 1e-10;
-        while ((upper - lower) > tol) {
-            double fc = rpeval(v, c*xsf);
-            double fd = rpeval(v, d*xsf);
-            if (fc > fd) {
-                upper = d;
-                d = c;
-                c = upper - phi*(upper - lower);
-            } else {
-                lower = c;
-                c = d;
-                d = lower + phi*(upper - lower);
-            }
-        }
-        return 0.5*(upper + lower);
-    }
-    
     const vector<Sample>& get_data(void) const {
         return data;
     }
     
-    bool has_poles(const VectorXd& v) {
-        double xmin=1e50;
-        double xmax=-1e50;
-        for (size_t i=0; i < data.size(); i++) {
-            xmin = min(data[i].x, xmin);
-            xmax = max(data[i].x, xmax);
-        }
-        // ensure the bounds are slightly wider than the actual data
-        double span=xmax - xmin;
-        xmin -= pscale*span;
-        xmax += pscale*span;
-        
-        // now compute roots of bottom polynomial
-        switch(order_m) {
-        case 0:
-            return false; // cannot have poles
-        case 1:
-            {
-                double pole = -1 / v[order_n+1];
-                
-                /*
-                if (pole >= xmin && pole <= xmax) {
-                    printf("pole at %lf (v=%lf)\n", -1/v[order_n+1], v[order_n+1]);
-                }
-                */
-                
-                return pole >= xmin && pole <= xmax;
-            }
-        case 2:
-            {
-                double a = v[order_n+2];
-                double b = v[order_n+1];
-                double c = base_value;
-                double sb = b < 0 ? -1 : 1;
-                double q = -0.5*(b + sb*sqrt(b*b - 4*a*c));
-                double pole1 = q/a;
-                double pole2 = c/q;
-                              
-                return (pole1 >= xmin && pole1 <= xmax) ||
-                       (pole2 >= xmin && pole2 <= xmax);
-            }
-        default:
-            // TODO: see NR chapter 5.6 for cubic roots
-            logger.error("Warning: no implementation to compute roots of order-%d polynomial\n",
-                order_m
-            );
-            return false;
-        };
-    }
+    double evaluate(VectorXd& v);
+    VectorXd evaluate_derivative(VectorXd& v);
+    VectorXd gauss_newton_direction(VectorXd& v, VectorXd& deriv, double& fsse);
+    VectorXd gauss_newton_armijo(VectorXd& v);
+    double peak(const VectorXd& v);
+    bool has_poles(const VectorXd& v);
     
     const vector<Sample>& data;
     int order_n;
